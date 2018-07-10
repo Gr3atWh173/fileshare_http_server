e6b99df6de6ea5641fe413e4c4918e0f5353ea93
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2018-07-10 16:40:54 +0530

    add get function

--- server.rb
+++ server.rb
@@ -2,9 +2,27 @@ require "webrick"
 
 class FileShareHTTPServer < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(request, response)
+    return unless is_safe(request.path)
+    if exists(request.path)
+      response.status = 200
+      response.body   = File.open(request.path, 'rb').read()
+    else
+      response.status = 404
+      response.body   = "File does not exist on server"
+    end
   end
 
   def do_POST(request, response)
+  end
+
+  private
+
+  def is_safe(path)
+    return true
+  end
+
+  def exists(path)
+    File.file? path
   end
 end
 
